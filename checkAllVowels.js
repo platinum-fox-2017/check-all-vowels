372a41536a01b80f6542ad9cf8198662856d03db
Ardhiansyah Putra <ardhimetanaputra@gmail.com>
2018-02-06 10:38:34 +0700

    Release 0: Selasa

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,12 @@
+function checkVowels(str) {
+	var vowels = ['a', 'e', 'i', 'o', 'u'];
+	var result = true;
 
+	for (var i = 0; i < str.length; i++) {
+		if (!str.includes(vowels[i])) return false;
+	}
+	return result;
+}
+
+console.log(checkVowels('aiueo'));
+console.log(checkVowels('katak'));
