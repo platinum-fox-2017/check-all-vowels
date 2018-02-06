a4f74d4578ae2d87861c3f3b367324d31067dfbb
srohimah <sitirohimach@gmail.com>
2018-02-06 13:52:10 +0700

    check All Vowels

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,15 @@
+const checkAllVowels = string=>{
+    let strToLower = string.toLowerCase();
+    let vocal = 'aiueo';
+    for (let i = 0; i<string.length; i++){
+        if(vocal.indexOf(strToLower[i]) === -1){
+            return false
+        }
+    }
+    return true
+}
 
+console.log(checkAllVowels('AIUoe'))
+console.log(checkAllVowels('AAAAAa'))
+console.log(checkAllVowels('AlUBCoe'))
+console.log(checkAllVowels('xUCCoe'))
