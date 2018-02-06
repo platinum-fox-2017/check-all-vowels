864dad95c27e8126dc25bff3b496c6e9559c1b23
Fitrul Islam <fitrul.islam@gmail.com>
2018-02-06 11:58:28 +0700

    selasa rabu done

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,3 +1,4 @@
+//SELASA
 function cekString(str){
   var bukanVokal = 'bcdfghjklmnpqrstvwxyz123456789'
   for(var i=0; i<str.length; i++){
@@ -14,3 +15,23 @@ console.log(cekString('aiuaoe')); //TRUE
 console.log(cekString('a3aa1')); //FALSE
 console.log(cekString('aaab')); //FALSE
 console.log(cekString('aaaAEo')); //TRUE
+
+console.log('------------------------');
+//RABU
+function arrDuaDimensi(kata){
+  var arr = [];
+  for(var i=0; i<3; i++){
+    var arrDalam = [];
+    for(var i=0; i<kata.length; i++){
+      if(kata[i]===','){
+        arr.push(arrDalam);
+        arrDalam = [];
+      } else {
+        arrDalam.push(kata[i]);
+      }
+    }
+  }
+  arr.push(arrDalam);
+  return arr;
+}
+console.log(arrDuaDimensi('aqrst,ukaei,ffooo'));
