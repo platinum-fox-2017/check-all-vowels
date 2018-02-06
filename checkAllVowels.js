9457a4042011f398e74db9a69cb9957cccdb05ed
Fadhilmch <fadhil.mochammad1095@gmail.com>
2018-02-06 11:06:00 +0700

    updated release 1

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -8,7 +8,7 @@ function checkAllVowels(str){
   let tempArray = str.toLowerCase().split("");
   for(var i=0;i<tempArray.length;i++){
     if(tempArray[i]!='a'&&tempArray[i]!='i'&&tempArray[i]!='u'&&tempArray[i]!='e'&&tempArray[i]!='o'){
-      return false
+      return false;
     }
   }
   return true;
@@ -16,3 +16,24 @@ function checkAllVowels(str){
 
 console.log(checkAllVowels("aiueo"));
 console.log(checkAllVowels("aiueodfetij"));
+
+function stringToArray(input){
+  let arr = input.split(",");
+  let result = generateArray(3);
+  for(var i = 0; i<3;i++){
+    for(var j = 0; j < 5; j++){
+      result[i].push(arr[i][j])
+    }
+  }
+  return result;
+}
+
+function generateArray (row){
+  let temp = [];
+  for(var i = 0; i < row ; i++){
+    temp.push([]);
+  }
+  return temp;
+}
+
+console.log(stringToArray("aqrst,ukaei,ffooo"));
