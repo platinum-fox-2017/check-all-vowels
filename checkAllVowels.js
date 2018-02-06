67a2e4b028567bd7ac0026039f2c37d3b49654fe
yosaid92 <yohanessahrul92@gmail.com>
2018-02-06 21:23:24 +0700

    add - checkVowels-challenge

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,19 @@
-
+function checkAllVowels(str) {
+    var count = 0;
+    var vowels = ['a','i','u','e','o'];
+    for(var i=0; i<vowels.length; i++){
+        for(var j=0; j<str.length; j++){
+            if(vowels[i] == str[j]){
+                count++;
+                break;
+            }
+        }
+    }
+    if(count == 5){
+        return true;
+    }
+    return false;
+}
+console.log(checkAllVowels('baiufegoh'));
+console.log(checkAllVowels('aiueo'));
+console.log(checkAllVowels('asiuExxo'));
