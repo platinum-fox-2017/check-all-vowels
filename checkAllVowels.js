d4d14c411bc27ce04170e87749db65b644654dc7
Kevin Himawan Hie <kevinhimawanhie@Kevins-MacBook-Pro.local>
2018-02-06 11:43:21 +0700

    allvowels

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,10 +1,21 @@
 const allVowels = (word)=>{
-    let kamusVowel = ['a','i','u','e','o', ' ']
+  let kamusVowel = ['a','i','u','e','o', ' ']
 
-    for(let i = 0; i < word.length;i++){
-      if(kamusVowel.indexOf(word[i]) === -1){
-        return false
-      }
+  let newWords = word.toLowerCase()
+
+  // for(let i of newWords){
+  //   if(kamusVowel.indexOf(i) === -1){
+  //     return false
+  //   }
+  // }
+
+  for(let i in newWords){
+    if(kamusVowel.indexOf(newWords[i]) === -1){
+      return false
     }
-    return true
+  }
+  return true
 }
+
+console.log(allVowels('aaa'))
+console.log(allVowels('AiuEo'))
