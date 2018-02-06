5dd485780d09007cf7340a72276abd37ef01b50b
root <irsyadpahlapi28@gmail.com>
2018-02-05 23:38:00 -0500

    upload checkallvowels js

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,12 @@
+function vowel(str){
+  var strkecil=str.toLowerCase();
+  var key='aiueo'
+  for(let i=0;i<str.length;i++){
+    if(key.includes(strkecil[i])===false){
+      return false
+    }
+  }
+  return true
+}
 
+console.log(vowel('aiuuuuuueo'))
