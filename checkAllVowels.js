4096b5515e500d0d573df790b9f70e14206be90a
Fitrul Islam <fitrul.islam@gmail.com>
2018-02-06 11:46:18 +0700

    check all vowels done

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,16 @@
+function cekString(str){
+  var bukanVokal = 'bcdfghjklmnpqrstvwxyz123456789'
+  for(var i=0; i<str.length; i++){
+    for(var j=0; j<bukanVokal.length; j++){
+      if(str[i]===bukanVokal[j]){
+        return false;
+      }
+    }
+  }
+  return true;
+}
 
+console.log(cekString('aiuaoe')); //TRUE
+console.log(cekString('a3aa1')); //FALSE
+console.log(cekString('aaab')); //FALSE
+console.log(cekString('aaaAEo')); //TRUE
