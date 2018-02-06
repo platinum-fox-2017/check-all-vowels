acb3785d7b29e787788bb462c4a92a0ca71c7ca7
Herby Herado <herby,herado@gmail.com>
2018-02-06 10:17:00 +0700

    Release 0

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,13 @@
+function checkVowels(kata){
+    var count = 0;
+    for (i = 0; i < kata.length; i++){
+        lower = kata[i].toLowerCase();
+        if (lower == 'a' || lower == 'i' || lower == 'u' || lower == 'e' || lower == 'o'){
+            count++;
+        }
+    }
+    return (kata.length == count)?  true : false;
+}
 
+console.log(checkVowels('aAIueo')) // true
+console.log(checkVowels('aAIuexo')) // false
