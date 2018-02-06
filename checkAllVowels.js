521287c72d1014735cfe908caad1877eae416df8
davidyunius <32894219+davidyunius@users.noreply.github.com>
2018-02-06 14:45:32 +0700

    release 1

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,17 +1,10 @@
-'use strict'
-
-function checkAllVowel(string) {
-    let vowel = ['a', 'i', 'u', 'e', 'o',' '];
-    for (let str of string) {
-        if (vowel.indexOf(str.toLowerCase()) === -1) {
-            return false
-            // debugger
-        }
-    }
-    return true;
+function twoDimensionArray(string) {
+     let splitString = string.split(',')
+     let result = []
+     for (let i = 0; i < splitString.length; i++) {
+        result.push(splitString[i].split(''))
+     }
+     return result
 }
 
-console.log(checkAllVowel('o e u i a'))
-console.log(checkAllVowel('kevin'))
-console.log(checkAllVowel('gu st Av'))
-console.log(checkAllVowel('uUe Ai aA'))
+console.log(twoDimensionArray('aqrst,ukaei,ffooo'))
