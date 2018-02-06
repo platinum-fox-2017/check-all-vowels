61c1773b12c8b4fed8b6984309575ac54af161c7
haripermadi <harinurpermadi@gmail.com>
2018-02-06 11:58:37 +0700

    release 0

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,13 @@
-
+const checkVowel = str =>{
+    const strlowercase = str.toLowerCase()
+    const vowel = ' aiueo'
+    for(let i = 0; i< strlowercase.length; i++){
+        let indexVowel = vowel.indexOf(strlowercase[i])
+        if(indexVowel === -1){
+            return false
+        }
+    }
+    return true
+}
+console.log(checkVowel('aiO eo'))
+console.log(checkVowel('aiobEo'))
