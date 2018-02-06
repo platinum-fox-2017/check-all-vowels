fd9f040054bbaff4491d4fec702369e95014317e
Reza Pramudhika <rezapramudhika@gmail.com>
2018-02-06 11:39:48 +0700

    Release 0

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,23 @@
+function checkVowels(str){
+    var vowels = ['a', 'i', 'u', 'e', 'o'];
+    var strToArr = str.split('');
+    var isVowels = 0;
 
+    for(var i=0; i<str.length; i++){
+        for(var j=0; j<=vowels.length; j++){
+            if(str[i]==vowels[j]){
+                isVowels += 1;
+            }
+        }
+    }
+    
+    if(isVowels !== str.length){
+        return false;
+    }
+    return true;
+}
+
+console.log(checkVowels('eoaiu'));
+console.log(checkVowels('aaiibiuu'));
+console.log(checkVowels('ieuooaaji'));
+console.log(checkVowels('oiaeiooaaii'));
