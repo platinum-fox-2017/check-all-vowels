e98c78b78bf56e5931e0e327b102fe746033b8b5
Fadhilmch <fadhil.mochammad1095@gmail.com>
2018-02-06 10:50:49 +0700

    finished release 0

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,18 @@
+function checkAllVowels(str){
+  let tempArray = str.toLowerCase().split("");
+  let result = tempArray.reduce((hasil,value) => value|(value!='a'&&value!='i'&&value!='u'&&value!='e'&&value!='o')?false:true)
+  return result;
+}
 
+function checkAllVowels(str){
+  let tempArray = str.toLowerCase().split("");
+  for(var i=0;i<tempArray.length;i++){
+    if(tempArray[i]!='a'&&tempArray[i]!='i'&&tempArray[i]!='u'&&tempArray[i]!='e'&&tempArray[i]!='o'){
+      return false
+    }
+  }
+  return true;
+}
+
+console.log(checkAllVowels("aiueo"));
+console.log(checkAllVowels("aiueodfetij"));
