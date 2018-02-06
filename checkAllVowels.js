3c0c47502ed6e4fec8d417619d82f5c47b2df38c
Kevin Himawan Hie <kevinhimawanhie@Kevins-MacBook-Pro.local>
2018-02-06 11:30:57 +0700

    vowels

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,10 @@
+const allVowels = (word)=>{
+    let kamusVowel = ['a','i','u','e','o', ' ']
 
+    for(let i = 0; i < word.length;i++){
+      if(kamusVowel.indexOf(word[i]) === -1){
+        return false
+      }
+    }
+    return true
+}
