d2a35017e587d3fe0232f43b32ad267ec40fc335
Haidar Afif Maulana <haidarafifmaulana@gmail.com>
2018-02-06 11:49:48 +0700

    Function checkAllVowels -- Haidar

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,24 @@
+function checkAllVowels(input){
+   input = input.toLowerCase();
+   var vocal = ['a','i','u','e','o'];
+   var isVocal = true;
+   i = 0;
+   while(isVocal){
+      var cekVokal = vocal.indexOf(input[i]);
+      debugger
+      if(cekVokal < 0){
+         return false
+      }
+      if(i === input.length - 1){
+        isVocal = false;
+        return true;
+      }
+      debugger
+      i++
+   }
+    
+}
 
+console.log(checkAllVowels('aiueo'));
+console.log(checkAllVowels('anbiueo'));
+console.log(checkAllVowels('Anbiueo'));
