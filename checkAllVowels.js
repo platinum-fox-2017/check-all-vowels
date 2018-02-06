8709258b390518505b4e170a450c43cfad78ecb6
marcosumali <marco.sumali90@gmail.com>
2018-02-06 15:05:06 +0700

    add hasil

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,41 @@
 
+function checkVowels(str) {
+
+    var vowels = ['a', 'i', 'u', 'e', 'o'];
+    var vowelsPoint = 0;
+    var strSplit = str.split('');
+    // console.log(strSplit.length); //['a','b','c','d','e'] // 5
+
+    for (var j = 0; j < vowels.length; j++) {
+        for (var i = 0; i < strSplit.length; i++) {
+            if (strSplit[i] == vowels[j]) {
+                vowelsPoint += 1;
+                debugger
+                break;
+            }
+            
+        }
+    }
+        
+
+    if (vowelsPoint < strSplit.length) {
+        return false;
+    } else {
+        return true;
+    }
+
+    /*
+    a vs a : true,next
+    b vs a : false, next
+    b vs i : false, next
+    b vs u : false, next
+    b vs e : false, next
+    b vs o : false, END: false
+
+    */
+
+
+}
+
+
+console.log(checkVowels('abcde'));
