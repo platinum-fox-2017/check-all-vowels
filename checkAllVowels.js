ca38fc46c9c8026c6343fb5a2fc672b00a504abc
Fachrizal Gita <Cecha@Cechas-MacBook-Pro.local>
2018-02-06 11:53:20 +0700

    release 0

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,14 @@
+function checkAllVowels(words){
+  const vowels = ['a', 'i', 'u', 'e', 'o'];
 
+  for(let i=0; i<words.length; i++){
+    // console.log(`vowels ${vowels.indexOf(words[i])}`);
+    if(vowels.indexOf(words[i]) === -1){
+      return false
+    }
+  }
+  return true
+}
+
+console.log(checkAllVowels('qwerty'));
+console.log(checkAllVowels('aiueo'));
