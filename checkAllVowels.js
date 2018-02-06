88f7f79ead02ac51ebaf4e2d57c57ae2e2ddec26
unknown <fatimah.azzhr@gmail.com>
2018-02-06 12:58:18 +0700

    release 1

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -10,6 +10,24 @@ function isVowels(str) {
   return true;
 }
 
+function boxedStrings(str) {
+  str = str.replace(/,/g, "");
+  var arrStr = str.split("");
+  var box = [];
+  var row = 3;
+  var column = 5;
+  var count = 0;
+  for (var i = 0; i < row; i++) {
+    box.push([]);
+    for (var j = 0; j < column; j++) {
+      box[i].push(arrStr[count]);
+      count++;
+    }
+  }
+
+  return box;
+}
+
 // TEST CASE
 
 console.log(isVowels("aiueo")); // true
@@ -19,3 +37,5 @@ console.log(isVowels("eeieei")); // true
 console.log(isVowels("ASNASNnn")); // false
 console.log(isVowels("EIEIEOUE")); // true
 console.log(isVowels("EAoeieiAE")); // true
+
+console.log(boxedStrings("aqrst,ukaei,ffooo"));
